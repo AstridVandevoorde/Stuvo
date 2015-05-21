875fa86e24860e4cc0f51dbeffd4c89c34cee00c
AstridVandevoorde <Astrid.Vandevoorde@hotmail.be>
2015-05-21 09:25:26 +0200

    Campus keuze

---
+++ CampusSelectie/dynamic.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.campus').click(function(){
+        $('.campus').css({"border-color":"white"});    
+          $(this).css({ "border-color" : "red"});
+    });
+});
