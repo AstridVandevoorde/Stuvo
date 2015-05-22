e8e78ac8f75751ad79558e26c8f1b83f112eb293
AstridVandevoorde <Astrid.Vandevoorde@hotmail.be>
2015-05-22 16:10:07 +0200

    update

--- Resto/dynamic.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.bolletje').click(function(){
-        $('.bolletje').css({"background-color":"rgb(220,220,220)"});    
-          $(this).css({ "background-color" : "red"});
-    });
-});
