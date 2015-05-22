765c0308b91a14a97042a99fc0ae5a2f1bd513c1
lennert66 <lennert.machiels@student.ehb.be>
2015-05-22 09:26:47 +0200

    www

---
+++ www/Script/javascript.js
@@ -0,0 +1,15 @@
+/* jshint
+browser: true,
+devel: true,
+jquery: true
+*/
+
+$(document).ready(function () {
+
+  $('.nav_knop').on('click', function () {
+    $('#container').slideToggle();
+
+  });
+
+
+});
