0c6ebb64962d220d9c8e602242a7bf8e49bb202f
AstridVandevoorde <Astrid.Vandevoorde@hotmail.be>
2015-05-22 16:09:11 +0200

    update

--- CampusSelectie/dynamic.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.campus').click(function(){
-        $('.campus').css({"border-color":"white"});    
-          $(this).css({ "border-color" : "red"});
-    });
-});
---
+++ dynamic.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.campus').click(function(){
+        $('.campus').css({"border-color":"white"});    
+          $(this).css({ "border-color" : "red"});
+    });
+       $('.bolletje').click(function(){
+        $('.bolletje').css({"background-color":"rgb(220,220,220)"});    
+          $(this).css({ "background-color" : "red"});
+    });
+    });
