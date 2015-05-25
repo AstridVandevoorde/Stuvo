9985ae6322f8986f23af127d570ef6434d9a4861
lennert66 <lennert.machiels@student.ehb.be>
2015-05-25 11:08:00 +0200

    www

--- www/javascript.js
+++ www/javascript.js
@@ -8,8 +8,15 @@ $(document).ready(function () {
 
   $('.nav_knop').on('click', function () {
     $('#container').slideToggle();
-
   });
-
+  
+  $('.campus').click(function () {
+    $('.campus').css({
+      "border-color": "white"
+    });
+    $(this).css({
+      "border-color": "red"
+    });
+  });
 
 });
