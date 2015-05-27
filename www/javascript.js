182e1873c231131f9a19c6513ddeb6164dd5c0cc
Pixelatex <ewardsomers@Ewards-MacBook-Pro.local>
2015-05-27 14:22:10 +0200

    Menu fix - Resto

--- www/javascript.js
+++ www/javascript.js
@@ -5,9 +5,26 @@ jquery: true
 */
 
 $(document).ready(function () {
+  var menu_active = false;
 
   $('.nav_knop').on('click', function () {
     $('#container').slideToggle();
+    if(menu_active == false){
+      menu_active == true;
+      if($("#wrapper").length === 0){
+        console.log("bestaat niet");
+      }else{
+        $('#wrapper').css("z-index",-40);
+      }
+    }else{
+      menu_active == false;
+      if($("#wrapper").length === 0){
+        console.log("bestaat niet");
+      }else{
+        $('#wrapper').css("z-index","inherit");
+      }
+    }
+
   });
   
   $('.campus').click(function () {
